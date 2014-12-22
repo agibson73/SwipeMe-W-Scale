e258b5ccd072e342a48965a3e0940ba989dad2f0
agibson73 <AGibson@Stevens-MacBook-Pro.local>
2014-12-21 19:05:33 -0500

    Initial Commit

---
+++ SwipeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeMe
+//
+//  Created by Steven Gibson on 12/21/14.
+//  Copyright (c) 2014 OakmontTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
